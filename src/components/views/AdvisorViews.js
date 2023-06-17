9ce212c0d77e25a1566a1a6e68cd30e157e37975
Jonathan VanDuyne <jonathanvanduyne@gmail.com>
2023-06-17 15:20:00 -0400

    landing page css

--- src/components/views/AdvisorViews.js
+++ src/components/views/AdvisorViews.js
@@ -1,43 +1,58 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { LandingPageGreeting } from "../LandingPage/LandingPage.js"
-import { Profile } from "../Profile/Profile.js"
-import { UpdateAdvisorProfile } from "../Profile/AdvisorEditProfile.js"
-import { AdvisorPolicyList} from "../AdvisorPolicies/AdvisorPoliciesList.js"
-import { AdvisorCustomerDetail} from "../AdvisorPolicies/AdvisorCustomerDetail.js"
-import { ProductList } from "../Products/ProductList.js"
-import { AddNewPolicy } from "../AdvisorPolicies/AddNewPolicy.js"
-import { AddNewProduct } from "../Products/AddNewProduct.js"
-
+import { Outlet, Route, Routes } from "react-router-dom";
+import { LandingPageGreeting } from "../LandingPage/LandingPage.js";
+import { Profile } from "../Profile/Profile.js";
+import { UpdateAdvisorProfile } from "../Profile/AdvisorEditProfile.js";
+import { AdvisorPolicyList } from "../AdvisorPolicies/AdvisorPoliciesList.js";
+import { AdvisorCustomerDetail } from "../AdvisorPolicies/AdvisorCustomerDetail.js";
+import { ProductList } from "../Products/ProductList.js";
+import { AddNewPolicy } from "../AdvisorPolicies/AddNewPolicy.js";
+import { AddNewProduct } from "../Products/AddNewProduct.js";
+import "./Views.css";
 
 export const AdvisorViews = () => {
 	return (
-		<Routes>
-			<Route path="/" element={
-				<>
-					<h1>💰Money Honey🍯</h1>
-					<h2>Connecting With a Financial Advisor Has Never Been So Sweet</h2>
+		<div className="center-container">
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<section className="hero is-primary center-content">
+								<div className="container">
+									<h1 className="title">Money Honey</h1>
+									<h2 className="subtitle">
+										Connecting With a Financial Advisor Has Never Been So Sweet
+									</h2>
+									<h3 className="greeting">
+										<LandingPageGreeting />
+									</h3>
+								</div>
+							</section>
+							
+							<section>
+								<Outlet />
+							</section>
+						</>
+					}
+				/>
 
-					<LandingPageGreeting />
-					<Outlet />
-				</>
-			} />
+				<Route path="profile" element={<Profile />} />
 
-			<Route path="profile" element={<Profile />} />
-			
-			<Route path="profile/UpdateAdvisorProfile" element={<UpdateAdvisorProfile />} />
-			
-			<Route path="/policies" element={<AdvisorPolicyList />} />
-			
-			<Route path=":customerId" element={<AdvisorCustomerDetail />} />
-			
-			<Route path="/AddNewPolicy" element={<AddNewPolicy />} />
-			
-			<Route path="/products" element={<ProductList />} />
-			
-			<Route path="/AddNewProduct" element={<AddNewProduct />} />
+				<Route
+					path="profile/UpdateAdvisorProfile"
+					element={<UpdateAdvisorProfile />}
+				/>
 
-			
+				<Route path="/policies" element={<AdvisorPolicyList />} />
 
-		</Routes>
-	)
-}
+				<Route path=":customerId" element={<AdvisorCustomerDetail />} />
+
+				<Route path="/AddNewPolicy" element={<AddNewPolicy />} />
+
+				<Route path="/products" element={<ProductList />} />
+
+				<Route path="/AddNewProduct" element={<AddNewProduct />} />
+			</Routes>
+		</div>
+	);
+};
--- src/components/views/CustomerViews.js
+++ src/components/views/CustomerViews.js
@@ -1,38 +1,54 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { LandingPageGreeting } from "../LandingPage/LandingPage.js"
-import { Profile } from "../Profile/Profile.js"
-import { UpdateCustomerProfile } from "../Profile/CustomerEditProfile.js"
-import { CustomerPolicyList } from "../CustomerPolicies/CustomerPoliciesList.js"
-import { CustomerAdvisorDetail } from "../CustomerPolicies/CustomerAdvisorDetail.js"
-import { AddNewBeneficiary } from "../Profile/CustomerAddBeneficiary.js"
-import { CustomerEditBeneficiary} from "../Profile/CustomerEditBeneficiary.js"
-
+import { Outlet, Route, Routes } from "react-router-dom";
+import { LandingPageGreeting } from "../LandingPage/LandingPage.js";
+import { Profile } from "../Profile/Profile.js";
+import { UpdateCustomerProfile } from "../Profile/CustomerEditProfile.js";
+import { CustomerPolicyList } from "../CustomerPolicies/CustomerPoliciesList.js";
+import { CustomerAdvisorDetail } from "../CustomerPolicies/CustomerAdvisorDetail.js";
+import { AddNewBeneficiary } from "../Profile/CustomerAddBeneficiary.js";
+import { CustomerEditBeneficiary } from "../Profile/CustomerEditBeneficiary.js";
+import "./Views.css";
 
 export const CustomerViews = () => {
 	return (
-		<Routes>
-			<Route path="/" element={
-				<>
-					<h1>💰🍯Money Honey🍯💰</h1>
-					<h2>Connecting With a Financial Advisor Has Never Been So Sweet</h2>
+		<div className="center-container">
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<section className="hero is-primary center-content">
+								<div className="container">
+									<h1 className="title">Money Honey</h1>
+									<h2 className="subtitle">
+										Connecting With a Financial Advisor Has Never Been So Sweet
+									</h2>
+									<h3 className="greeting">
+										<LandingPageGreeting />
+									</h3>
+								</div>
+							</section>
 
-					<LandingPageGreeting />
-					<Outlet />
-				</>
-			} />
+							<section>
+								<Outlet />
+							</section>
+						</>
+					}
+				/>
 
-			<Route path="profile" element={<Profile />} />
+				<Route path="profile" element={<Profile />} />
 
-			<Route path="profile/UpdateCustomerProfile" element={<UpdateCustomerProfile />} />
+				<Route path="profile/UpdateCustomerProfile" element={<UpdateCustomerProfile />}
+				/>
 
-			<Route path="policies" element={<CustomerPolicyList />} />
+				<Route path="policies" element={<CustomerPolicyList />} />
 
-			<Route path=":advisorId" element={<CustomerAdvisorDetail />} />
-			
-			<Route path="addNewBeneficiary" element={<AddNewBeneficiary />} />
-			
-			<Route path="profile/editBeneficiary/:beneficiaryId" element={<CustomerEditBeneficiary />} />
+				<Route path=":advisorId" element={<CustomerAdvisorDetail />} />
 
-		</Routes>
-	)
-}
+				<Route path="addNewBeneficiary" element={<AddNewBeneficiary />} />
+
+				<Route path="profile/editBeneficiary/:beneficiaryId" element={<CustomerEditBeneficiary />}
+				/>
+			</Routes>
+		</div>
+	);
+};
