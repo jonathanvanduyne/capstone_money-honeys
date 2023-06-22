d1cce90ebf48aa29861dd28ce467107bff9b768d
Jonathan VanDuyne <jonathanvanduyne@gmail.com>
2023-06-21 20:42:39 -0400

    customer navbar css

--- src/components/nav/CustomerNavBar.js
+++ src/components/nav/CustomerNavBar.js
@@ -1,38 +1,78 @@
-import { Link, useNavigate } from "react-router-dom"
-import "./NavBar.css"
-import { getCurrentCustomerInfo } from "../../APIManager.js"
-import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom";
+import "./NavBar.css";
+import { getCurrentCustomerInfo } from "../../APIManager.js";
+import { useEffect, useState } from "react";
 
 export const CustomerNavBar = () => {
-    const navigate = useNavigate()
-    const [currentCustomer, setCurrentCustomer] = useState([])
+    const navigate = useNavigate();
+    const [currentCustomer, setCurrentCustomer] = useState([]);
+    const [isExpanded, setIsExpanded] = useState(false);
 
     useEffect(() => {
-            
         const fetchData = async () => {
+            const customerNavBarName = await getCurrentCustomerInfo();
+            setCurrentCustomer(customerNavBarName);
+        };
 
-                const customerNavBarName = await getCurrentCustomerInfo()
-                setCurrentCustomer(customerNavBarName)
-            }
-            
-            fetchData()
-        }, [])
+        fetchData();
+    }, []);
+
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const handleLinkClick = () => {
+        setIsExpanded(false);
+    };
 
     return (
-        <ul className="navbar">
-            <li className="navbar__locations">
-                <Link className="navbar__link" to="/profile">{currentCustomer?.user?.firstName}'s Profile</Link>
-            </li>
-            <li className="navbar__locations">
-                <Link className="navbar__link" to="/policies">Policies</Link>
-            </li>
-
-            <li className="navbar__item navbar__logout">
-                <Link className="navbar__link" to="" onClick={() => {
-                    localStorage.removeItem("moneyHoneys_user")
-                    navigate("/", { replace: true })
-                }}>Logout</Link>
-            </li>
-        </ul>
-    )
-}
+        <div
+            className={`navbar__container ${isExpanded ? "expanded" : ""}`}
+            onMouseEnter={() => setIsExpanded(true)}
+            onMouseLeave={() => setIsExpanded(false)}
+        >
+            <div
+                className={`navbar__icon ${isExpanded ? "active" : ""}`}
+                onClick={handleToggle}
+            >
+                <span className="navbar__icon-bar"></span>
+                <span className="navbar__icon-bar"></span>
+                <span className="navbar__icon-bar"></span>
+            </div>
+            {isExpanded ? (
+                <ul>
+                    <li className="navbar__locations">
+                        <Link
+                            className="navbar__link"
+                            to="/profile"
+                            onClick={handleLinkClick}
+                        >
+                            {currentCustomer?.user?.firstName}'s Profile
+                        </Link>
+                    </li>
+                    <li className="navbar__locations">
+                        <Link
+                            className="navbar__link"
+                            to="/policies"
+                            onClick={handleLinkClick}
+                        >
+                            Policies
+                        </Link>
+                    </li>
+                    <li className="navbar__locations">
+                        <Link
+                            className="navbar__link"
+                            to=""
+                            onClick={() => {
+                                localStorage.removeItem("moneyHoneys_user");
+                                navigate("/", { replace: true });
+                            }}
+                        >
+                            Logout
+                        </Link>
+                    </li>
+                </ul>
+            ) : null}
+        </div>
+    );
+};
