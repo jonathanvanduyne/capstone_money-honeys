d5fb0a40b6e4f8e1e6b61edc683ad6a0c23626dd
Jonathan VanDuyne <jonathanvanduyne@gmail.com>
2023-06-30 05:42:08 -0400

    v1 of working graphs, not yet modal

--- src/components/Investments/AdvisorInvestments/GraphModal.js
+++ src/components/Investments/AdvisorInvestments/GraphModal.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getHistoricalDayClosingPrice } from "../../../APIManager.js";
-import { Line } from "react-chartjs-2";
+import { format } from "date-fns";
+import { CanvasJSChart } from "canvasjs-react-charts";
 
 export const InvestmentGraphModal = ({ investment }) => {
     const [stockData, setStockData] = useState([]);
@@ -8,7 +9,9 @@ export const InvestmentGraphModal = ({ investment }) => {
     useEffect(() => {
         const fetchStockData = async () => {
             try {
-                const historicalData = await getHistoricalDayClosingPrice(investment.stockSymbol.stockMarketSymbol);
+                const historicalData = await getHistoricalDayClosingPrice(
+                    investment.stockSymbol.stockMarketSymbol
+                );
                 setStockData(historicalData);
             } catch (error) {
                 console.error("Error fetching stock data:", error);
@@ -19,52 +22,77 @@ export const InvestmentGraphModal = ({ investment }) => {
     }, [investment]);
 
     const formatData = (data) => {
-        const investmentAnniversaries = stockData.reduce((anniversaries, entry) => {
-            const entryYear = new Date(entry.date).getFullYear();
-            if (!anniversaries.includes(entryYear)) {
-                anniversaries.push(entryYear);
-            }
-            return anniversaries;
-        }, []);
-
-        return data
-            .filter((entry) => {
-                const entryYear = new Date(entry.date).getFullYear();
-                return investmentAnniversaries.includes(entryYear);
-            })
-            .map((entry) => ({
-                x: new Date(entry.date),
-                y: entry.close,
-            }));
+        return data.map((entry) => ({
+            date: new Date(entry.date),
+            close: entry.close,
+        }));
     };
 
-    const chartData = {
-        datasets: [
-            {
-                label: "Closing Price",
-                data: formatData(stockData),
-                borderColor: "#8884d8",
-                fill: false,
+    const chartData = formatData(stockData);
+
+    const renderChart = () => {
+        const options = {
+            theme: "light2",
+            title: {
+                text: "Investment Performance",
             },
-        ],
-    };
-
-    const chartOptions = {
-        scales: {
-            y: {
-                title: {
-                    text: "Price",
+            rangeSelector: {
+                inputFields: {
+                    enabled: false,
                 },
-                type: "linear", // Add this line to specify the scale type as linear
+                buttons: [
+                    {
+                        range: 30, // 30 days
+                        label: "1M",
+                        rangeType: "day",
+                    },
+                    // Add other range buttons as needed
+                ],
+                buttonClick: (e) => {
+                    const rangeType = e.dataPoint.rangeType;
+                    const range = e.dataPoint.range;
+                    const newStart = new Date();
+                    newStart.setDate(newStart.getDate() - range);
+
+                    // Fetch new data based on the selected range
+                    fetchHistoricalData(newStart, new Date(), rangeType);
+                },
             },
-        },
+            data: [
+                {
+                    type: "line",
+                    xValueFormatString: "YYYY-MM-DD",
+                    yValueFormatString: "$#,##0.00",
+                    dataPoints: chartData.map((entry) => ({
+                        x: entry.date,
+                        y: entry.close,
+                    })),
+                },
+            ],
+        };
+
+        return <CanvasJSChart options={options} />;
+    };
+
+    const fetchHistoricalData = async (start, end, rangeType) => {
+        try {
+            // Use the start and end dates to fetch historical data
+            const historicalData = await getHistoricalDayClosingPrice(
+                investment.stockSymbol.stockMarketSymbol,
+                format(start, "yyyy-MM-dd"),
+                format(end, "yyyy-MM-dd")
+            );
+            setStockData(historicalData);
+        } catch (error) {
+            console.error("Error fetching stock data:", error);
+        }
     };
 
     return (
         <>
             <h3>Investment Performance</h3>
             {stockData.length > 0 ? (
-                <Line data={chartData} options={chartOptions} />
+                renderChart()
             ) : (
                 <p>Loading investment data...</p>
             )}
